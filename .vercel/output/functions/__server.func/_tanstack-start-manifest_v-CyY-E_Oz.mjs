deb331f68f3617b3601bb10af8981f6d2f8bc405
ZXH Official <zulqarnainhaider7116@gmail.com>
2026-09-20 07:50:21 +0500

    unzipgit

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CyY-E_Oz.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CyY-E_Oz.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/workspace/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-9xJwTvF4.js", "/assets/rolldown-runtime-CbXtAM7H.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-9xJwTvF4.js"
+		} }]
+	},
+	"/": {
+		filePath: "/workspace/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-ChsSdzpa.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
